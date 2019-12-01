94eaec1d5285e2ef1e519c943c2b7d7f1cb0e92f
Dara Carolan <daraclare@gmail.com>
2019-12-01 18:57:01 +0000

    error page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { Beers } from "../components/Beers/Beers";
 import { NextPage } from "next";
 import styled from "styled-components";
 import { getBeers } from "../api/";
+import { Error } from "./_error";
 
 interface Props {
   beers: any[];
@@ -15,9 +16,7 @@ const StyledSection = styled.section`
 `;
 
 const Home: NextPage<Props> = ({ beers }) => (
-  <StyledSection>
-    <Beers beers={beers} />
-  </StyledSection>
+  <StyledSection>{beers ? <Beers beers={beers} /> : <Error />}</StyledSection>
 );
 
 Home.getInitialProps = async () => {
